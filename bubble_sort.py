681571bd6e703b14681bc4e8cac594c147dccaf4
Yanzhishang <1176377464@qq.com>
2017-03-10 02:16:09 +0800

    。

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,7 @@
 import random
 #定义三个常量用来表示下限、上限和数组长度
 start = 1
-stop = 100
+stop = 1000
 length = 100
 #定义一个新数组存放随机数
 list = []
@@ -18,7 +18,6 @@ i = 5
 for i in range(len(list) - 1):   #循环次数 n - 2 次
     for j in range(len(list) - i - 1):
         if list[j] > list[j + 1]:
-            #list[j],list[j + 1] = list[j + i],list[j]  #元组的方式
             tmp = list[j]
             list[j] = list[j + 1]
             list[j + 1] = tmp
